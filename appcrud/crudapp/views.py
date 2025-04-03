e24e18780dcccc6dcf8bf1ba9a8d2b08fed5ebda
cordovabarbara <aliendocordova@gmail.com>
2025-04-03 18:14:49 -0500

    feat(views): agregar vista task_list_and_create, renderiza plantilla task_list.html

--- appcrud/crudapp/views.py
+++ appcrud/crudapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def task_list_and_create(request):
+    form = TaskForm()
+    tasks = Task.objects.all()
+
+    return render(request, 'task_list.html',{
+        'form':form,
+        'task':tasks
+    })
