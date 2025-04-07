81a535634c0e2bfb499e48632107a2ddb226fae0
cordovabarbara <aliendocordova@gmail.com>
2025-04-07 18:23:26 -0500

    fix(migrations): aplicar migraciones pendientes para crud

--- appcrud/appcrud/settings.py
+++ appcrud/appcrud/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crudapp.apps.CrudConfig',
+    'crud.apps.CrudConfig',
 ]
 
 MIDDLEWARE = [
