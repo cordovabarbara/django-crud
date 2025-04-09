b5d94ce1a4a3c8e68781b855c6fd4c183bbb870f
cordovabarbara <aliendocordova@gmail.com>
2025-04-09 18:30:28 -0500

    feat(views):Añadir manejo de método POST para guardar tareas

--- appcrud/crud/views.py
+++ appcrud/crud/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
 def task_list_and_create(request):
-    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('crud:crud_list')
+    else:
+        form = TaskForm()
     tasks = Task.objects.all()
 
     return render(request, 'task_list.html',{
