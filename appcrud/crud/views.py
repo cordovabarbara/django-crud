aaf2aa232acf211e3a9ced8f71d62c2e6796cee6
cordovabarbara <aliendocordova@gmail.com>
2025-04-09 19:02:19 -0500

    feat(views): filtrar tareas por estado completado/incompleto

--- appcrud/crud/views.py
+++ appcrud/crud/views.py
@@ -11,9 +11,13 @@ def task_list_and_create(request):
             return redirect('crud:crud_list')
     else:
         form = TaskForm()
-    tasks = Task.objects.all()
+    #tasks = Task.objects.all()
+    complete_tasks = Task.objects.filter(is_completed=True)
+    incomplete_tasks = Task.objects.filter(is_completed=False)
 
     return render(request, 'task_list.html',{
         'form':form,
-        'tasks':tasks
+        #'tasks':tasks
+        'complete_tasks':complete_tasks,
+        'incomplete_tasks':incomplete_tasks
     })
