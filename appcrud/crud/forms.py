8f8ef1dbd307fdb517ebf906b1aab39765673958
cordovabarbara <aliendocordova@gmail.com>
2025-04-18 19:03:32 -0500

    feat: Agrega funciones para editar y eliminar tareas

--- appcrud/crud/forms.py
+++ appcrud/crud/forms.py
@@ -5,3 +5,8 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows':3})
+
+        }
